61d3835aa71f5de01027d68e67cd0be915c4a228
JW <jw00000@gmail.com>
2025-11-11 00:04:27 +0100

    Adjust scripts to be more similar. Will also be easier to integrate with Home Assistant web hooks.

--- sds-011.py
+++ sds-011.py
@@ -12,10 +12,16 @@ TIMEZONE = pytz.timezone('Europe/Brussels')
 sensor = SDS011('/dev/ttyUSB0')
 sensor.set_work_period(work_time=WAIT_PERIOD)
 
-while True:
+def fetch_once():
     now = datetime.datetime.now().astimezone(TIMEZONE)
     nowStr = now.strftime('%Y-%m-%dT%H:%M:%S%z')
     (pm25, pm10) = sensor.query()
     print("%s,%s,%s" % (nowStr, pm25, pm10))
     sys.stdout.flush()
-    time.sleep(WAIT_PERIOD * 60)
+
+def fetch_loop():
+    while True:
+        main()
+        time.sleep(WAIT_PERIOD * 60)
+
+fetch_once()
--- sds-011.sh
+++ sds-011.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 sds-011.py | tee -a sds-011.log
+while true; do python3 sds-011.py | tee -a sds-011.log; sleep 30m; done
